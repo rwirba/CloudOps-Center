3fcd18e774ba71ac7a10ff09379785906fe12356
rwirba <admin@ymail.com>
2025-04-09 17:32:55 +0000

    modified:   frontend/src/components/ManageAWSResources.js

--- frontend/src/components/ManageAWSResources.js
+++ frontend/src/components/ManageAWSResources.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 function ManageAWSResources() {
   const [instances, setInstances] = useState([]);
   const [users, setUsers] = useState([]);
+  const [showInstances, setShowInstances] = useState(false);
+  const [showUsers, setShowUsers] = useState(false);
 
   useEffect(() => {
     axios.get('/api/instances').then(res => {
@@ -16,27 +18,41 @@ function ManageAWSResources() {
 
   return (
     <div>
-      <h3 style={{ color: '#FF9900' }}>EC2 Instances</h3>
-      <ul>
-        {instances.map(instance => (
-          <li key={instance.InstanceId}>
-            {instance.InstanceId} - {instance.State.Name}
-            <button onClick={() => axios.post('/api/start', { instanceId: instance.InstanceId })}>Start</button>
-            <button onClick={() => axios.post('/api/stop', { instanceId: instance.InstanceId })}>Stop</button>
-            <button onClick={() => axios.post('/api/terminate', { instanceId: instance.InstanceId })}>Terminate</button>
-          </li>
-        ))}
-      </ul>
+      <h3
+        onClick={() => setShowInstances(!showInstances)}
+        style={{ color: '#FF9900', cursor: 'pointer' }}
+      >
+        EC2 Instances
+      </h3>
+      {showInstances && (
+        <ul>
+          {instances.map(instance => (
+            <li key={instance.InstanceId}>
+              {instance.InstanceId} - {instance.State.Name}
+              <button onClick={() => axios.post('/api/start', { instanceId: instance.InstanceId })}>Start</button>
+              <button onClick={() => axios.post('/api/stop', { instanceId: instance.InstanceId })}>Stop</button>
+              <button onClick={() => axios.post('/api/terminate', { instanceId: instance.InstanceId })}>Terminate</button>
+            </li>
+          ))}
+        </ul>
+      )}
 
-      <h3 style={{ color: '#4C51BF' }}>IAM Users</h3>
-      <ul>
-        {users.map(user => (
-          <li key={user.UserName}>
-            {user.UserName}
-            <button onClick={() => axios.post('/api/rotate-access-key', { userName: user.UserName })}>Rotate Key</button>
-          </li>
-        ))}
-      </ul>
+      <h3
+        onClick={() => setShowUsers(!showUsers)}
+        style={{ color: '#4C51BF', cursor: 'pointer' }}
+      >
+        IAM Users
+      </h3>
+      {showUsers && (
+        <ul>
+          {users.map(user => (
+            <li key={user.UserName}>
+              {user.UserName}
+              <button onClick={() => axios.post('/api/rotate-access-key', { userName: user.UserName })}>Rotate Key</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
