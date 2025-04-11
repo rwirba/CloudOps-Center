1148cd3f429add0292bb1be2c7c5387d137046cc
Ryan <rwirba@protonmail.com>
2025-04-11 04:02:02 +0000

    update display for cloudwatch

--- frontend/src/components/EC2Table.jsx
+++ frontend/src/components/EC2Table.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button, Paper } from '@mui/material';
+import CloudWatchChart from './CloudWatchChart';
 
 function EC2Table({ instances, onAction }) {
   return (
-    <TableContainer component={Paper} sx={{ mb: 4 }}>
+    <TableContainer component={Paper} sx={{ mt: 2 }}>
       <Table>
         <TableHead>
           <TableRow>
@@ -12,27 +13,28 @@ function EC2Table({ instances, onAction }) {
             <TableCell>Type</TableCell>
             <TableCell>Status</TableCell>
             <TableCell>Public IP</TableCell>
-            <TableCell align="right">Actions</TableCell>
+            <TableCell>Actions</TableCell>
+            <TableCell>CloudWatch</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {instances.map((instance) => {
-            const nameTag = instance.Tags?.find(tag => tag.Key === 'Name');
-            return (
-              <TableRow key={instance.InstanceId}>
-                <TableCell>{nameTag?.Value || 'Unnamed'}</TableCell>
-                <TableCell>{instance.InstanceId}</TableCell>
-                <TableCell>{instance.InstanceType}</TableCell>
-                <TableCell>{instance.State.Name}</TableCell>
-                <TableCell>{instance.PublicIpAddress || 'N/A'}</TableCell>
-                <TableCell align="right">
-                  <Button onClick={() => onAction('start', instance.InstanceId)}>Start</Button>
-                  <Button onClick={() => onAction('stop', instance.InstanceId)}>Stop</Button>
-                  <Button onClick={() => onAction('terminate', instance.InstanceId)}>Terminate</Button>
-                </TableCell>
-              </TableRow>
-            );
-          })}
+          {instances.map((instance, i) => (
+            <TableRow key={i}>
+              <TableCell>{instance.Tags?.find(t => t.Key === 'Name')?.Value || 'N/A'}</TableCell>
+              <TableCell>{instance.InstanceId}</TableCell>
+              <TableCell>{instance.InstanceType}</TableCell>
+              <TableCell>{instance.State?.Name}</TableCell>
+              <TableCell>{instance.PublicIpAddress || 'N/A'}</TableCell>
+              <TableCell>
+                <Button size="small" onClick={() => onAction('start', instance.InstanceId)}>Start</Button>
+                <Button size="small" onClick={() => onAction('stop', instance.InstanceId)}>Stop</Button>
+                <Button size="small" onClick={() => onAction('terminate', instance.InstanceId)}>Terminate</Button>
+              </TableCell>
+              <TableCell>
+                <CloudWatchChart instanceId={instance.InstanceId} />
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </TableContainer>
